4fe0f02a779451dc0cd338d4d2930b353120c14b
Cole Mickens <cole.mickens@gmail.com>
2024-05-08 16:05:11 -0700

    test: CI + flakehub cache upload completion

--- flake.nix
+++ flake.nix
@@ -1,9 +1,34 @@
 {
   description = "flakesrock - the coolest flake in town";
 
-  inputs = { };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
 
   outputs = inputs: {
     truth = builtins.toFile "flakes-desc" "totally rad";
+
+    # NOTE(colemickens): include both since I have aarch64-linux dev machine for now
+
+    packages = {
+      aarch64-linux =
+        let
+          pkgs = import inputs.nixpkgs { system = "aarch64-linux"; };
+        in {
+          default = pkgs.runCommand "test-flakehub-cache-payload" {} ''
+            set -x
+            dd if=/dev/random of=$out bs=1M count=512
+          '';
+        };
+      x86_64-linux =
+        let
+          pkgs = import inputs.nixpkgs { system = "aarch64-linux"; };
+        in {
+          default = pkgs.runCommand "test-flakehub-cache-payload" {} ''
+            set -x
+            dd if=/dev/random of=$out bs=1M count=512
+          '';
+        };
+    };
   };
 }
